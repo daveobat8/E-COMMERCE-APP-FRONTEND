fa7d946ddf6884cb35194eb0974510a9aa3af439
David <dave.obat@student.moringashool.com>
2024-10-01 12:33:24 +0300

    Navbar created and styled

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <nav id="desktop-nav">
+        <div className="logo">
+          Nunua <div> <img src="logo.png" alt="Cart" className=" logo_icon" /></div>
+        </div>
+        <div>
+          <ul className="nav-links">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#shop">Shop</a>
+            </li>
+            <li>
+              <a href="#products">Products</a>
+            </li>
+            <li>
+              <a href="#blog">Blog</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+            <li>
+              <a href="#search">
+                <img src="search.png" alt="Cart" className="icon" />
+              </a>
+            </li>
+            <li>
+              <a href="#cart">
+                <img src="cart.png" alt="Cart" className="icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav id="hamburger-nav">
+        <div className="logo">Nunua <div> <img src="logo.png" alt="Cart" className=" logo_icon" /></div></div>
+        <div className="hamburger-menu">
+          <div className="cart-links">
+          <li>
+              <a href="#search">
+                <img src="search.png" alt="Cart" className="icon" />
+              </a>
+            </li>
+            <li>
+              <a href="#cart">
+                <img src="cart.png" alt="Cart" className="icon" />
+              </a>
+            </li></div>
+            <div
+            className={`hamburger-icon ${menuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={`menu-links ${menuOpen ? "open" : ""}`}>
+            <li>
+              <a href="#about" onClick={toggleMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#shop" onClick={toggleMenu}>
+                Shop
+              </a>
+            </li>
+            <li>
+              <a href="#products" onClick={toggleMenu}>
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={toggleMenu}>
+                Contact Us
+              </a>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
