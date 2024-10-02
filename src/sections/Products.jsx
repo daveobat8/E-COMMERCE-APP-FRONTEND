050bf9c590960447a3271cb7e189535886ee0429
David <dave.obat@student.moringashool.com>
2024-10-02 12:12:26 +0300

    products displayed as cards in products page

--- src/sections/Products.jsx
+++ src/sections/Products.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 function Products() {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/products")
       .then((response) => response.json())
-      .then((products) => {
-        console.log(products);
+      .then((data) => {
+
+        setProducts(data)
+        console.log(data);
       })
       .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
   return (
     <>
+    <div className="container">
+        {products.map((product,index)=> (
+            <div className="card" key={index}>
+                <img className="product-image" src={product.image_url} />
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>Price: {product.price}</p>
+            </div>
+        ))}
+    </div>
     </>
   );
 }
